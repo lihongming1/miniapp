ca465e9c560bf290c71eb05397edcb78a802d565
lihongming <lihongming@dankegongyu.com>
2019-05-09 11:18:23 +0800

    master

--- src/main/java/com/wx/miniapp/dao/model/LoginUser.java
+++ src/main/java/com/wx/miniapp/dao/model/LoginUser.java
@@ -3,4 +3,17 @@ package com.wx.miniapp.dao.model;
 import java.io.Serializable;
 
 public class LoginUser implements Serializable {
+
+    private String openId;
+    private String nickName;
+    private String gender;
+    private String language;
+    private String city;
+    private String province;
+    private String country;
+    private String avatarUrl;
+    private String unionId;
+    private String sessionKey;
+
+
 }
