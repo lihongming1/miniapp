33e52bec811f9278bd0655cfcf23de957c72433f
lihongming <lihongming@dankegongyu.com>
2019-05-10 12:05:34 +0800

    master

---
+++ src/main/java/com/wx/miniapp/controller/MessageController.java
@@ -0,0 +1,16 @@
+package com.wx.miniapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 消息模版
+ */
+@RestController
+@RequestMapping(value = "/com/wx/message")
+public class MessageController {
+
+
+
+
+}
