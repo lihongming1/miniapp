dff0ebdb6b5959a90a1165a88f8ae67252427635
lihongming <lihongming@dankegongyu.com>
2019-05-10 13:21:05 +0800

    master

--- src/main/java/com/wx/miniapp/config/wxjava/WxMiniappConfig.java
+++ src/main/java/com/wx/miniapp/config/wxjava/WxMiniappConfig.java
@@ -1,8 +1,9 @@
-package com.wx.miniapp.config.authorize;
+package com.wx.miniapp.config.wxjava;
 
-
+import cn.binarywang.wx.miniapp.api.WxMaMsgService;
 import cn.binarywang.wx.miniapp.api.WxMaService;
 import cn.binarywang.wx.miniapp.api.WxMaUserService;
+import cn.binarywang.wx.miniapp.api.impl.WxMaMsgServiceImpl;
 import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
 import cn.binarywang.wx.miniapp.api.impl.WxMaUserServiceImpl;
 import cn.binarywang.wx.miniapp.config.WxMaInMemoryConfig;
@@ -12,21 +13,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class WxMaUserConfig {
+public class WxMiniappConfig {
 
     @Autowired
     private ApplicationConfig applicationConfig;
 
     @Bean
-    public WxMaUserService wxMaUserService(){
+    public WxMaService wxMaService() {
         WxMaService wxMaService = new WxMaServiceImpl();
         WxMaInMemoryConfig wxMaConfig = new WxMaInMemoryConfig();
         wxMaConfig.setAppid(applicationConfig.appid);
         wxMaConfig.setSecret(applicationConfig.secret);
         wxMaService.setWxMaConfig(wxMaConfig);
+        return wxMaService;
+    }
+
+    @Bean
+    public WxMaUserService wxMaUserService() {
+        WxMaService wxMaService = wxMaService();
         WxMaUserService wxMaUserService = new WxMaUserServiceImpl(wxMaService);
         return wxMaUserService;
     }
 
+    @Bean
+    public WxMaMsgService wxMaMsgService() {
+        WxMaService wxMaService = wxMaService();
+        WxMaMsgService wxMaMsgService = new WxMaMsgServiceImpl(wxMaService);
+        return wxMaMsgService;
+    }
 
 }
