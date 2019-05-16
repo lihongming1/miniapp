48f3f604f6f441c590568ae01a4f075c02174fdb
lihongming <lihongming@dankegongyu.com>
2019-05-16 15:32:40 +0800

    master

--- src/main/java/com/wx/miniapp/controller/CodeController.java
+++ src/main/java/com/wx/miniapp/controller/CodeController.java
@@ -3,14 +3,11 @@ package com.wx.miniapp.controller;
 import cn.binarywang.wx.miniapp.api.WxMaQrcodeService;
 import cn.binarywang.wx.miniapp.bean.WxMaCodeLineColor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStream;
 
